61922889fc817a00cabfdde9845c542140174dcd
Wookis <chanuk9531@daum.net>
2024-07-30 11:40:24 +0900

    #200-test2 내가 찐이야

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -9,8 +9,7 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		//#1 -TEST EDIT
 		SpringApplication.run(DemoApplication.class, args);
-		//#3 - TEST-RE-EDIT
-		//#5 - TEST-RERE-EDIT
+		//#200 - 내가 찐이야
 	}
 
 }
