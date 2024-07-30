62138edfa5d14b128b6b49d66be612d7bd459ddf
Wookis <chanuk9531@daum.net>
2024-07-30 11:39:47 +0900

    #100-test 초기화

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,9 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		//#1 -TEST EDIT
 		SpringApplication.run(DemoApplication.class, args);
-		//#200 - 내가 찐이야
 	}
 
 }
