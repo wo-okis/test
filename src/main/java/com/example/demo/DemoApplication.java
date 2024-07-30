261d73538ebc3562e21bac9be4987fd2fccfd462
Wookis <chanuk9531@daum.net>
2024-07-30 11:49:46 +0900

    test2-by-add

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
+		//#test2-by-ad
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
